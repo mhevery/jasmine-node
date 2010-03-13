f2ebe78f86899b5339128bff8c3e716bbdae53b8
Matthew Kitt <mk.kitt@gmail.com>
2010-03-12 22:05:30 -0700

    Minor formatting on the output to the console, included test duration, summary and stack trace on failing tests. Added an optional 'isVerbose' parameter to executeSpecsInFolder, when set to true it will log and print passing tests. Added arguments of 'runner' and 'log' to the 'done' callback.

--- specs.js
+++ specs.js
@@ -4,7 +4,11 @@ var sys = require('sys');
 
 process.mixin(global, jasmine);
 
-jasmine.executeSpecsInFolder('spec', function(){
-  sys.puts('DONE');
-  process.exit(0);
+jasmine.executeSpecsInFolder('spec', function(runner, log){
+//  sys.puts('DONE');
+  var results = runner.results();
+  if(results.failedCount === 0)
+        process.exit(0);
+    else
+        process.exit(1);
 });
