8927bc42f2325b7ae511a0518fc2ef5597a32b57
Matthew Kitt <mk.kitt@gmail.com>
2010-03-29 15:31:15 -0600

    removed the deprecated process.mixin from specs.js

--- specs.js
+++ specs.js
@@ -2,7 +2,11 @@ require.paths.push("./lib");
 var jasmine = require('jasmine');
 var sys = require('sys');
 
-process.mixin(global, jasmine);
+//process.mixin(global, jasmine);
+
+for(var key in jasmine) {
+  global[key] = jasmine[key];
+}
 
 jasmine.executeSpecsInFolder('spec', function(runner, log){
   process.exit(runner.results().failedCount);
