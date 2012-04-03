82d6a5c8369671bfe8517d5f43a4b9d93c75f868
Matthew Smith <matthew.smith2@extendhealth.com>
2012-04-03 14:01:53 -0600

    Removed this keyword, as it was causing a test failure. Added coffee option to scripts/specs bash test runner

--- spec/async-callback_spec.js
+++ spec/async-callback_spec.js
@@ -28,7 +28,7 @@ describe('async-callback', function() {
     it("should accept timeout for individual spec", function() {
       env.describe("it", function() {
         env.it("doesn't wait", function(done) {
-          this.expect(1+2).toEqual(3);
+          expect(1+2).toEqual(3);
         }, 250);
       });
 
