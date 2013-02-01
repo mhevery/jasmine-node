207fc6a22162413f45a8ba4b17ad01aaa179e776
Chris Moultrie <cmoultrie@endgames.us>
2013-02-01 13:59:29 -0500

    Fixed tabbing issue

--- spec/TestSpec.js
+++ spec/TestSpec.js
@@ -3,3 +3,16 @@ describe('jasmine-node-flat', function(){
     expect(1+2).toEqual(3);
   });
 });
+describe("Top level describe block", function() {
+      it("first it block in top level describe", function() {
+                expect(true).toEqual(true);
+                  });
+        describe("Second level describe block", function() {
+                it("first it block in second level describe", function() {
+                           expect(true).toBe(true);
+                               });
+                  });
+          it("second it block in top level describe", function() {
+                    expect(true).toEqual(true);
+                      });
+});
