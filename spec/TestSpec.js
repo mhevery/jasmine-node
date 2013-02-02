441a00114b62b4ab07d122ef6c335cb99a2c976e
Chris Moultrie <cmoultrie@endgames.us>
2013-02-02 14:23:27 -0500

    Fixed autotest watch issue where it only triggered once.

--- spec/TestSpec.js
+++ spec/TestSpec.js
@@ -45,15 +45,15 @@ describe('root', function () {
 });
 
 describe("Top level describe block", function() {
-      it("first it block in top level describe", function() {
-                expect(true).toEqual(true);
-                  });
-        describe("Second level describe block", function() {
-                it("first it block in second level describe", function() {
-                           expect(true).toBe(true);
-                               });
-                  });
-          it("second it block in top level describe", function() {
-                    expect(true).toEqual(true);
-                      });
+  it("first it block in top level describe", function() {
+    expect(true).toEqual(true);
+  });
+  describe("Second level describe block", function() {
+    it("first it block in second level describe", function() {
+      expect(true).toBe(true);
+    });
+  });
+  it("second it block in top level describe", function() {
+    expect(true).toEqual(true);
+  });
 });
