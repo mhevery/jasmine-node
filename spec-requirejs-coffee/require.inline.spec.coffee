2e6670f469fd4be026a7567b4fa5df3259020db5
Chris Moultrie <cmoultrie@endgames.us>
2013-02-02 12:22:25 -0500

    Fixed requirejs timer mock, updated specs count
    Resolves #171

--- spec-requirejs-coffee/require.inline.spec.coffee
+++
@@ -1,8 +0,0 @@
-define "formal", ->
-  class Formal
-    constructor: (@object) ->
-        # nada
- 
-require ["formal"], (F) ->
-  describe "Formal", ->
-    expect(F.stuff()).toEqual 5
--- specs.sh
+++ specs.sh
@@ -6,14 +6,14 @@ echo "Running all tests located in the spec directory"
 command=$entry"spec"
 echo $command
 time $command #/nested/uber-nested
-echo -e "\033[1;35m--- Should have 50 tests and 84 assertions and 2 Failure. ---\033[0m"
+echo -e "\033[1;35m--- Should have 52 tests and 91 assertions and 2 Failure. ---\033[0m"
 echo ""
 
 echo "Running all tests located in the spec directory with coffee option"
 command=$entry"--coffee spec"
 echo $command
 time $command #/nested/uber-nested
-echo -e "\033[1;35m--- Should have 54 tests and 88 assertions and 4 Failures. ---\033[0m"
+echo -e "\033[1;35m--- Should have 56 tests and 95 assertions and 4 Failures. ---\033[0m"
 echo ""
 
 echo "Running all tests located in the spec directory with requirejs option"
@@ -21,7 +21,7 @@ echo "Running all tests located in the spec directory with requirejs option"
 command=$entry"--runWithRequireJs spec"
 echo $command
 time $command
-echo -e "\033[1;35m--- Should have 50 tests and 84 assertions and 1 Failure. ---\033[0m"
+echo -e "\033[1;35m--- Should have 52 tests and 91 assertions and 2 Failure. ---\033[0m"
 echo ""
 
 echo "Running all tests located in the spec-requirejs directory with requirejs, requirejs setup, and coffee option"
