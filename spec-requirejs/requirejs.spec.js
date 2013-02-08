d7301b86bb3c90a6ea1c9e3e2d1728be0e239ce6
Chris Moultrie <cmoultrie@endgames.us>
2013-02-08 15:30:41 -0500

    Added a beforeEach and afterEach to verify the additions

--- spec-requirejs/requirejs.spec.js
+++ spec-requirejs/requirejs.spec.js
@@ -1,5 +1,12 @@
 require(['requirejs.sut'], function(sut){
   describe('RequireJs basic tests', function(){
+    beforeEach(function(){
+        expect(true).toBeTruthy();
+    });
+    afterEach(function(){
+        expect(true).toBeTruthy();
+    });
+    
     it('should load sut', function(){
       expect(sut.name).toBe('Subject To Test');
       expect(sut.method(2)).toBe(3);
