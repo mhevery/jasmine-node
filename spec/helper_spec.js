ddb5210b55b80b609b714e4937597f24abdd35be
Chris Moultrie <cmoultrie@endgame.com>
2014-03-05 16:33:10 -0500

    Got rid of helper option, because jasmine doesn't like that

--- spec/helper_spec.js
+++ spec/helper_spec.js
@@ -1,27 +1,30 @@
 describe("helper", function() {
     beforeEach(function() {
-        jasmine.addMatchers({toHaveProperty: function(util, customEqualityTesters) {
-            return {
-                compare: function(actual, expected) {
-                    var result = {};
+        jasmine.addMatchers(
+            {
+                toHaveProperty: function(util, customEqualityTesters) {
+                    return {
+                        compare: function(actual, expected) {
+                            var result = {};
 
-                    try {
-                        result.pass = expected in actual;
-                    }
-                    catch (e) {
-                        result.pass = false;
-                    }
+                            try {
+                                result.pass = expected in actual;
+                            }
+                            catch (e) {
+                                result.pass = false;
+                            }
 
-                    if (result.pass) {
-                        result.message = "Expected " + actual + " not to be a property.";
-                    } else {
-                        result.message = "Expected " + actual + " to be a property.";
-                    }
+                            if (result.pass) {
+                                result.message = "Expected " + actual + " not to be a property.";
+                            } else {
+                                result.message = "Expected " + actual + " to be a property.";
+                            }
 
-                    return result
+                            return result
+                        }
+                    }
                 }
-            }
-        }});
+            });
     });
     it("should load the helpers", function() {
         expect(expect(true).toHaveProperty).toEqual(jasmine.any(Function));
