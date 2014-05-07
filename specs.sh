bbefb64cd986f87a9e447b72da5ae7e767e67749
Kevin Horvatin <Kevin.Horvatin@software.dell.com>
2014-05-07 09:34:37 -0500

    Add additional tests and code in the _contains block to prevent the exception that happens when an undefined variable. This exception causes grunt-jasmine-node to fail

--- specs.sh
+++ specs.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
-entry="node lib/jasmine-node/cli.js --noStack "
+entry="node lib/jasmine-node/cli.js "
 
 echo "Running all tests located in the spec directory"
 command=$entry"spec"
 echo $command
 time $command #/nested/uber-nested
-echo -e "\033[1;35m--- Should have 59 tests and 104 assertions and 4 Failure. ---\033[0m"
+echo -e "\033[1;35m--- Should have 65 tests and 110 assertions and 8 Failures. ---\033[0m"
 echo ""
 
 echo "Running all tests located in the spec directory with coffee option"
 command=$entry"--coffee spec"
 echo $command
 time $command #/nested/uber-nested
-echo -e "\033[1;35m--- Should have 64 tests and 109 assertions and 6 Failures. ---\033[0m"
+echo -e "\033[1;35m--- Should have 70 tests and 115 assertions and 10 Failures. ---\033[0m"
+
+echo "Running all tests located in the spec directory with forceExit option"
+command=$entry"--forceexit spec"
+echo $command
+time $command #/nested/uber-nested
+echo -e "\033[1;35m--- Should have 65 tests and 110 assertions and 8 Failures. ---\033[0m"
 echo ""
 
 echo "Running all tests located in the spec directory with requirejs option"
@@ -21,17 +27,17 @@ echo "Running all tests located in the spec directory with requirejs option"
 command=$entry"--runWithRequireJs spec"
 echo $command
 time $command
-echo -e "\033[1;35m--- Should have 59 tests and 104 assertions and 4 Failure. ---\033[0m"
+echo -e "\033[1;35m--- Should have 65 tests and 110 assertions and 8 Failures. ---\033[0m"
 echo ""
 
 echo "Running all tests located in the spec-requirejs directory with requirejs, requirejs setup, and coffee option"
 command=$entry"--runWithRequireJs --requireJsSetup spec-requirejs-coffee/requirejs-setup.js --coffee spec-requirejs-coffee"
 echo $command
 time $command
-echo -e "\033[1;35m--- Should have 2 tests and 4 assertions and 0 Failure. ---\033[0m"
+echo -e "\033[1;35m--- Should have 2 tests and 4 assertions and 0 Failures. ---\033[0m"
 
 echo "Running three specs file in the spec directory with coffee option"
 command=$entry"--coffee spec/AsyncSpec.coffee spec/CoffeeSpec.coffee spec/SampleSpecs.js"
 echo $command
 time $command
-echo -e "\033[1;35m--- Should have 3 tests and 3 assertions and 2 Failure. ---\033[0m"
+echo -e "\033[1;35m--- Should have 3 tests and 3 assertions and 2 Failures. ---\033[0m"
